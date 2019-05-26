9fd12e23aca4fe776afa5868a3342616a9ff0231
Anthony Durot <gibioctet@gmail.com>
2019-05-26 19:16:06 +0200

    Small changes

--- routes/urlshorten.js
+++ routes/urlshorten.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const log = require('log4js').getLogger();
+const createError = require('http-errors');
 const urlshortenService = require('./services/urlshortenService');
 
 log.level = 'debug';
@@ -11,23 +12,35 @@ router.use(logger('dev'));
 router.use(express.json());
 
 /* GET code */
-router.get('/:hash', async (req, res) => {
+router.get('/:hash', async (req, res, next) => {
   const { hash } = req.params;
   if (hash) {
     const item = await urlshortenService.read({ hash });
     return res.redirect(item.url);
   }
-  return res.status(404).send();
+  return next(createError(404, 'This url code does not exist'));
 });
 
 /* POST code */
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
   const { url } = req.body;
   if (url) {
-    const item = await urlshortenService.write({ url });
-    return res.json(item).send();
+    // Teste tout d'abord si l'url n'a pas déja été hashée
+    let urlDoc = await urlshortenService.read({ url }).catch((err) => {
+      log.error(err.stack);
+      next(createError(500, 'Une erreur interne est survenue'));
+    });
+    if (urlDoc) {
+      // Si l'url est déja trouvée
+      return res.json(urlDoc.hash).send();
+    }
+    urlDoc = await urlshortenService.write({ url }).catch((err) => {
+      log.error(err.stack);
+      next(createError(500, 'Une erreur interne est survenue'));
+    });
+    return res.json(urlDoc.hash).send();
   }
-  return res.status(404).send();
+  return next(createError(400, 'La requête est mal formatée'));
 });
 
 module.exports = router;
